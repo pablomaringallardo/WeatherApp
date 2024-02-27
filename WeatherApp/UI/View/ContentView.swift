c0941234db0ceec64ec66ef1ff1bf3530bfbaa4c
Pablo Marín <pablomaringallardo17@gmail.com>
2024-02-27 13:39:59 +0100

    add color palette and organize folder structure

---
+++ WeatherApp/UI/View/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Pablo Marín Gallardo on 27/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
